4643e0846fb88743df8d821f0545aad23ead3314
Vinay Joshi <vinay01joshi@gmail.com>
2018-07-08 21:30:39 +0530

    returing additional claims from IDP

--- ImageGallery/src/ImageGallery.Client/Startup.cs
+++ ImageGallery/src/ImageGallery.Client/Startup.cs
@@ -46,6 +46,7 @@ namespace ImageGallery.Client
                  options.Scope.Add("profile");
                  options.SaveTokens = true;
                  options.ClientSecret = "secret";
+                 options.GetClaimsFromUserInfoEndpoint = true;
              });
         }
 
