c21cdf1795898b7ea91e8f1e8fce625ad390104e
Vinay Joshi <vinay01joshi@gmail.com>
2018-07-24 18:55:39 +0530

    revoking access .

--- ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
+++ ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -189,6 +189,51 @@ namespace ImageGallery.Client.Controllers
 
         public async Task Logout()
         {
+            // get the metadata
+            var discoveryClient = new DiscoveryClient("https://localhost:44326/");
+            var metaDataResponse = await discoveryClient.GetAsync();
+
+            // create a TokenRevocationClient
+            var revocationClient = new TokenRevocationClient(
+                metaDataResponse.RevocationEndpoint,
+                "imagegalleryclient",
+                "secret");
+
+            // get the access token to revoke 
+            var accessToken = await HttpContext
+              .GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                var revokeAccessTokenResponse =
+                    await revocationClient.RevokeAccessTokenAsync(accessToken);
+
+                if (revokeAccessTokenResponse.IsError)
+                {
+                    throw new Exception("Problem encountered while revoking the access token."
+                        , revokeAccessTokenResponse.Exception);
+                }
+            }
+
+            // revoke the refresh token as well
+            var refreshToken = await HttpContext
+             .GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            if (!string.IsNullOrWhiteSpace(refreshToken))
+            {
+                var revokeRefreshTokenResponse =
+                    await revocationClient.RevokeRefreshTokenAsync(refreshToken);
+
+                if (revokeRefreshTokenResponse.IsError)
+                {
+                    throw new Exception("Problem encountered while revoking the refresh token."
+                        , revokeRefreshTokenResponse.Exception);
+                }
+            }
+
+
+            // Clears the  local cookie ("Cookies" must match name from scheme)
+
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
         }
